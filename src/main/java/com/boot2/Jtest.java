13da630b20532211a3269dd92fb26bbe41f8a484
giventadevelop <giventa.develop@gmail.com>
2025-10-29 12:39:50 -0400

    Jtest.java

--- src/main/java/com/boot2/Jtest.java
+++ src/main/java/com/boot2/Jtest.java
@@ -1,20 +1,27 @@
 package com.boot2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Jtest {
+
 
     public static void main(String[] args) {
         System.out.println("Hello World JTest");
 
+        Integer[] intArray = {1,1, 2,2,2 , 3,3,3,3} ;
+        List<Integer> numList = Arrays.asList(intArray);
 
+        Map<Integer, Long> countMap = numList.stream()
+                          .collect(Collectors.groupingBy(e ->e, Collectors.counting()));
 
+        System.out.println("fCount of Items : "+ countMap  );
 
+       /*MultiplierIntfc multiplyItems = (int a, int b ) -> a * b;
+       int multResult =  multiplyItems.multiplyItems(2,3);
+       System.out.println("funtional Interface multiplyItems : "+ multResult  );*/
     }
-
-
-/*    Input: s = "pwwkew"
-    Output: 3
-    Explanation: The answer is "wke", with the length of 3.
-    Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.*/
-
 
 }
